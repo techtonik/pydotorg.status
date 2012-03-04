0b5d12704731ca53326e90c46b093675e93adf14
anatoly techtonik <techtonik@gmail.com>
2012-03-05 00:58:00 +0300

    move the rest of the code into probe.py leaving only ui in status.py

--- probe.py
+++ probe.py
@@ -1,14 +1,26 @@
+'''
+Probe configured web-site, store result in DB and in memcache
+'''
+
+
 # -- config --
+
 TIMEOUT = 7  # seconds to wait until web service responds, 10 is AppEngine max
              # http://code.google.com/appengine/docs/python/urlfetch/fetchfunction.html
 SVC = 'Python Web Site'
 URL = 'http://python.org'
 
+
 # -- imports ---
 
+import logging
+import time
+from datetime import datetime, timedelta
+
+from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 
-from models import OK, FAIL, WARNING, UNKNOWN
+from models import OK, FAIL, WARNING, UNKNOWN, Sample
 
 # -- helpers --
 
@@ -19,7 +31,6 @@ def check(url, timeout=TIMEOUT):
       details contain additional data and
       latency is time taken by urlfetch() call in float seconds.
   '''
-  import time
   try:
     latency = time.time()
     result = urlfetch.fetch(URL, deadline=TIMEOUT)
@@ -36,4 +47,20 @@ def check(url, timeout=TIMEOUT):
 
 # -- main --
 
+def probe():
+  ''' Probe site, limit attempts to one per minute, return last probe '''
+  last_probe = memcache.get("last_probe")
+  if last_probe == None:
+    last_probe = Sample.all().order('-time').get()
+  if (last_probe == None or
+      datetime.now() - last_probe.time > timedelta(minutes=1)):
+    status, details, latency = check(URL)
+    last_probe = Sample(status=status, details=details, latency=latency)
+    last_probe.put()
+    if not memcache.set('last_probe', last_probe):
+      logging.error('Memcache set failed for last probe.')
+  return last_probe
+
+if __name__ == '__main__':
+  probe()
 
--- status.py
+++ status.py
@@ -7,14 +7,10 @@ from probe import SVC, URL
 from google.appengine.dist import use_library
 use_library('django', '1.2')
 
-from google.appengine.api import memcache
 from google.appengine.ext.webapp import template
 
-from datetime import datetime, timedelta
-import logging
-
 # -- helpers --
-from probe import check
+import probe
 
 # -- models ---
 from models import Sample
@@ -22,19 +18,7 @@ from models import Sample
 
 # -- the app --
 title = 'status of python.org services'
-
-
-# probe site, limit attempts to one per minute
-last_probe = memcache.get("last_probe")
-if last_probe == None:
-  last_probe = Sample.all().order('-time').get()
-if (last_probe == None or
-    datetime.now() - last_probe.time > timedelta(minutes=1)):
-  status, details, latency = check(URL)
-  last_probe = Sample(status=status, details=details, latency=latency)
-  last_probe.put()
-  if not memcache.set('last_probe', last_probe):
-    logging.error('Memcache set failed for last probe.')
+last_probe = probe.probe()
 
 # render stats for last 7 measurements
 probes = Sample.all().order('-time').fetch(limit=7)
